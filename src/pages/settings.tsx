adb1cc76083a843ce93055a5d453572578d4509c
Nicolas <nicolas@racchi.com>
2022-06-11 20:25:09 +0200

    feat: strategies + settings skeleton page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import Layout from "src/components/layout/Index";
+
+export default function Settings() {
+  return (
+    <Layout pageTitle="Settings">
+      <h1>Settings</h1>
+    </Layout>
+  );
+}
---
+++ src/pages/strategies.tsx
@@ -0,0 +1,9 @@
+import Layout from "src/components/layout/Index";
+
+export default function Strategies() {
+  return (
+    <Layout pageTitle="Strategies">
+      <h1>Strategies</h1>
+    </Layout>
+  );
+}
